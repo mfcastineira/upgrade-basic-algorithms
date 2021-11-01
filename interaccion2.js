2a2717df7ae0c4633740e409295b05bef8540714
Maria Fernanda Castineira <mfcastineira@gmail.com>
2021-11-01 17:05:35 +0100

    corregidos

--- interaccion2.js
+++ interaccion2.js
@@ -1,9 +1,8 @@
 // 1.1 cambia el valor de la propiedad age a 25//
 const character = {name: "jack Sparrow", age: 10};
-character["age"] 
-character["name"]
 
-character["age"]= 25
+character.ageage= 25
+console.log(character.age)
 //{name: 'Jack Sparrow', age: 25}
 
 //1.2mostrar en consola
@@ -11,17 +10,16 @@ const firtName= 'Jon';
 const lastName='Snow';
 const age= 24;
  
-console.log('Soy'+' '+firtName+' '+lastName+', tengo'+' '+age+'años'+' '+'y me gustan los lobos')
+console.log('Soy'+' '+firtName+' '+lastName+', tengo'+' '+age+'años'+' '+'y me gustan los lobos');
 
-// 1.3 imprime en consola la suma la suma del precio de ambos juguetes 
-const toy1 = {name: 'Buss myYear', price: 19}
-const toy2 = {name: 'Rallo Mcking', price: 29}
+// 1.3 imprime en consola la suma del precio de ambos juguetes 
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo Mcking', price: 29};
 
-toy1['price']
-toy2['price']
-const priceTotal= [toy1['price']+toy2['price']]
 
-console.log(priceTotal)
+const priceTotal= toy1.price + toy2.price;
+
+console.log(priceTotal);
 
 //actualiza el valor de la variable globalPrice y actualiza la propiedad finalPrice
 let globalBasePrice = 10000;
@@ -30,12 +28,11 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
 globalBasePrice= 2500;
 
-car1["finalPrice"]
-car2["finalPrice"]
-
 car1.finalPrice= car1.basePrice + globalBasePrice;
 car2.finalPrice= car2.basePrice + globalBasePrice;
 
+console.log(car1.finalPrice);
+console.log(car2.finalPrice);
 
 
 
